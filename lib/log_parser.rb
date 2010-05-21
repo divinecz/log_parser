586299d9595a4dc75d1703d70fbb8a90e5a72a54
Martin Hladil <martin@hladil.name>
2010-05-21 12:47:09 +0200

    refactoring

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,26 +1,27 @@
 class LogParser
 
-  def initialize(log_definitions_path, &read_byte_block)
+  def initialize(definitions_path, &read_byte_block)
     raise ArgumentError, "Missing block" unless block_given?
-    load_log_definitions(log_definitions_path)
+    load_definitions(definitions_path)
     @read_byte_block = read_byte_block
   end
 
   def parse
     header = read_byte
-    id = parse_log_id_from_header(header)
-    definition = find_log_definition(id)
-    data = [parse_log_data_from_header(header)]
-    data += read_log_data(definition)
-    puts data.size
+    id = parse_id_from_header(header)
+    definition = find_definition(id)
+    puts definition.inspect
+    raw_data = [parse_raw_data_from_header(header)]
+    raw_data += read_raw_data(definition["size"] - 1)
+    puts raw_data.size
     { :name => definition["name"] }
   end
 
   protected
 
-  def load_log_definitions(log_definitions_path)
-    @log_definitions = YAML::load(File.read(log_definitions_path))["logs"]
-    raise LogParserException, "Cannot load log definitions from #{log_definitions_path}" if @log_definitions.nil?
+  def load_definitions(definitions_path)
+    @definitions = YAML::load(File.read(definitions_path))["logs"]
+    raise LogParserException, "Cannot load log definitions from #{definitions_path}" if @definitions.nil?
     #TODO: validate
   end
 
@@ -28,26 +29,26 @@ class LogParser
     @read_byte_block.call
   end
 
-  def parse_log_id_from_header(header)
+  def parse_id_from_header(header)
     header & 0x7f
   end
 
-  def parse_log_data_from_header(header)
+  def parse_raw_data_from_header(header)
     header & 0x80
   end
 
-  def find_log_definition(log_id)
-    log_definition = @log_definitions[log_id]
-    raise LogParserException, "Unknown log definition for 0x#{log_id.to_s(16)}" if log_definition.nil?
-    log_definition
+  def find_definition(id)
+    definition = @definitions[id]
+    raise LogParserException, "Unknown log definition for 0x#{id.to_s(16)}" if definition.nil?
+    definition
   end
 
-  def read_log_data(log_definition)
-    data = []
-    (log_definition["size"] - 1).times do
-      data << read_byte
+  def read_raw_data(size)
+    raw_data = []
+    size.times do
+      raw_data << read_byte
     end
-    data
+    raw_data
   end
 
 end

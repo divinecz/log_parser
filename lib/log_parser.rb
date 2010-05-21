1c987fc894d7f84911d31d2730ef3a73fdf64c7c
Martin Hladil <martin@hladil.name>
2010-05-21 13:44:11 +0200

    draft again

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -11,10 +11,13 @@ class LogParser
     id = parse_id_from_header(header)
     definition = find_definition(id)
     puts definition.inspect
-    raw_data = [parse_raw_data_from_header(header)]
-    raw_data += read_raw_data(definition["size"] - 1)
-    puts raw_data.size
-    { :name => definition["name"] }
+    raw_data = parse_raw_data_from_header(header)
+    raw_data << read_raw_data(definition["size"] - 1)
+    attributes = {}
+    definition["attributes"].each_pair do |key, value|
+      attributes[key.to_sym] = type_cast_data(read_data(value["read"].to_s, raw_data), value["as"])
+    end
+    { :name => definition["name"], :attributes => attributes }
   end
 
   protected
@@ -22,7 +25,7 @@ class LogParser
   def load_definitions(definitions_path)
     @definitions = YAML::load(File.read(definitions_path))["logs"]
     raise LogParserException, "Cannot load log definitions from #{definitions_path}" if @definitions.nil?
-    #TODO: validate
+    #TODO: validate, regexp for read expression
   end
 
   def read_byte
@@ -34,7 +37,7 @@ class LogParser
   end
 
   def parse_raw_data_from_header(header)
-    header & 0x80
+    (header & 0x80).chr
   end
 
   def find_definition(id)
@@ -44,11 +47,31 @@ class LogParser
   end
 
   def read_raw_data(size)
-    raw_data = []
+    raw_data = ""
     size.times do
       raw_data << read_byte
     end
     raw_data
   end
-
+  
+  def read_data(expression, raw_data)
+    data = ""
+    terms = expression.split(" ")
+    terms.each do |term|
+      #TODO partial bytes
+      data << raw_data[term.to_i]
+    end
+    data
+  end
+  
+  def type_cast_data(data, as)
+    size = data.size
+    case as
+    when "bool"
+      data.unpack({1 => "C", 2 => "v", 4 => "I"}[size]).first > 0
+    when "uint"
+      data.unpack({1 => "C", 2 => "v", 4 => "I"}[size]).first
+    end
+  end
+  
 end

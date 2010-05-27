637e5745cadf078799ab913fd4617522b6b9fdfc
Martin Hladil <martin@hladil.name>
2010-05-27 15:45:40 +0200

    refactoring

--- lib/log_parser/parser.rb
+++ lib/log_parser/parser.rb
@@ -54,42 +54,48 @@ module LogParser
       attribute = Attribute.new
       ranges = expression.split(",")
       ranges.each do |range|
-        range.strip!
         range_start, range_end = range.split("-")
-        range_start.strip!
         if range_end.nil?
-          byte_index, bit_index = range_start.split(".")
-          byte_index = byte_index.to_i
-          if bit_index.nil?
-            attribute.append_byte(data[byte_index])
-          else
-            bit_index = bit_index.to_i
-            attribute.append_bit(data[byte_index][bit_index])
-          end
+          read_attribute_part(attribute, data, range_start)
         else
-          range_end.strip!
-          start_byte_index, start_bit_index = range_start.split(".")
-          end_byte_index, end_bit_index = range_end.split(".")
-          start_byte_index = start_byte_index.to_i
-          end_byte_index = end_byte_index.to_i
-          if start_bit_index.nil?
-            start_byte_index.upto(end_byte_index) do |byte_index|
-              attribute.append_byte(data[byte_index])
-            end
-          else
-            start_bit_index = start_bit_index.to_i
-            end_bit_index = end_bit_index.to_i
-            start_byte_index.upto(end_byte_index) do |byte_index|
-              current_bit_start_index = byte_index == start_byte_index ? start_bit_index : 0
-              current_bit_end_index = byte_index == end_byte_index ? end_bit_index : 7
-              current_bit_start_index.upto(current_bit_end_index) do |bit_index|
-                attribute.append_bit(data[byte_index][bit_index])
-              end
-            end
-          end
+          read_attribute_parts_range(attribute, data, range_start, range_end)
         end
       end
       attribute
     end
+
+    def read_attribute_part(attribute, data, index)
+      byte_index, bit_index = index.split(".")
+      byte_index = byte_index.to_i
+      if bit_index.nil?
+        attribute.append_byte(data[byte_index])
+      else
+        bit_index = bit_index.to_i
+        attribute.append_bit(data[byte_index][bit_index])
+      end
+    end
+
+    def read_attribute_parts_range(attribute, data, start_index, end_index)
+      start_byte_index, start_bit_index = start_index.split(".")
+      end_byte_index, end_bit_index = end_index.split(".")
+      start_byte_index = start_byte_index.to_i
+      end_byte_index = end_byte_index.to_i
+      if start_bit_index.nil?
+        start_byte_index.upto(end_byte_index) do |byte_index|
+          attribute.append_byte(data[byte_index])
+        end
+      else
+        start_bit_index = start_bit_index.to_i
+        end_bit_index = end_bit_index.to_i
+        start_byte_index.upto(end_byte_index) do |byte_index|
+          current_bit_start_index = byte_index == start_byte_index ? start_bit_index : 0
+          current_bit_end_index = byte_index == end_byte_index ? end_bit_index : 7
+          current_bit_start_index.upto(current_bit_end_index) do |bit_index|
+            attribute.append_bit(data[byte_index][bit_index])
+          end
+        end
+      end
+    end
+
   end
 end
